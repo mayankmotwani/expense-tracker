987e2d2f1ac486964bcd1073a585b279f4fbdefb
Mayank Motwani <76697579+mayankmotwani@users.noreply.github.com>
2024-01-18 00:01:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 
 require('./dbConnect')
 
@@ -7,17 +8,13 @@ const app = express()
 
 app.use(express.json())
 
+app.use(cors())
+
 const userRouter = require('./routes/usersRoute')
 app.use('/api/users/', userRouter)
 
 const transactionsRouter = require('./routes/transactionsRoute')
 app.use('/api/transactions/', transactionsRouter)
-
-app.use('/', express.static('client/build'))
-
-app.get('*' , (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-})
 
 const port = process.env.PORT || 5000
 app.listen(port, () => 
